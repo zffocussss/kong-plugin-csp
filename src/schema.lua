77a838f2bfebf70e31abfe717ba1cea8084c8cc1
focus zheng <zhengfei@ginolegaltech.cn>
2020-05-13 18:36:39 +0800

    debug

--- src/schema.lua
+++ src/schema.lua
@@ -1,11 +1,7 @@
 local typedefs = require "kong.db.schema.typedefs" 
 return {
-   name = "kong-plugin-scp",
+   no_consumer = true,
    fields = {
      abc = {type = "string", required = true}
-   },
-   no_consumer = true,
-   self_check = function(schema, plugin_t, dao, is_updating)
-     return true
-   end
+   }
 }
