0a1080c4a63775b5e17a83f5dda2c999bc902376
focus zheng <zhengfei@ginolegaltech.cn>
2020-05-13 17:54:53 +0800

    debug

--- src/schema.lua
+++ src/schema.lua
@@ -2,7 +2,7 @@
    name = "kong-plugin-scp",
    fileds = {
      abc = {type = "string", required = true
-   }
+   },
    no_consumer = true,
    self_check = function(schema, plugin_t, dao, is_updating)
      return true
