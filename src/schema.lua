4ff4e03cebbf8925adbc0e35115156d61bbcf376
focus zheng <zhengfei@ginolegaltech.cn>
2020-05-15 18:04:48 +0800

    update schema

--- src/schema.lua
+++ src/schema.lua
@@ -1,4 +1,18 @@
 local typedefs = require "kong.db.schema.typedefs" 
+
+local string_array = {
+  type = "array",
+  default = {},
+  elements = { type = "string" },
+}
+
+local test_para = {
+  type = "record",
+  fields = {
+    { test = string_array },
+  },
+}
+
 return {
    name = "kong-plugin-csp",
     fields = {
@@ -7,7 +21,7 @@ return {
     { config = {
         type = "record",
         fields = {
-          test = {type = "string", required = true},   
+          test = test_para,   
         },
       },
     },
