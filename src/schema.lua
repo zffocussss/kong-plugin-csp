0afbe4c8fbd0dacaa976df4b94186803dec07bb4
focus zheng <zhengfei@ginolegaltech.cn>
2020-05-15 17:05:23 +0800

    test schema configuration

--- src/schema.lua
+++ src/schema.lua
@@ -1,7 +1,15 @@
 local typedefs = require "kong.db.schema.typedefs" 
 return {
    name = "kong-plugin-csp",
-   fields = {
-     test = {type = "string", required = true},   
-   }
+    fields = {
+    { consumer = typedefs.no_consumer },
+    { protocols = typedefs.protocols_http },
+    { config = {
+        type = "record",
+        fields = {
+          test = {type = "string", required = true},   
+        },
+      },
+    },
+  },
 }
