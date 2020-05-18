7c3495a43dc789db5c090851fc46a4be32d50d81
focus zheng <zhengfei@ginolegaltech.cn>
2020-05-18 10:46:46 +0800

    update schema

--- src/schema.lua
+++ src/schema.lua
@@ -1,31 +1,68 @@
-local typedefs = require "kong.db.schema.typedefs" 
-
-local string_array = {
-  type = "array",
-  default = {},
-  elements = { type = "string" },
-}
-
-local test_para = {
-  type = "record",
-  fields = {
-    { test = string_array },
-  },
-}
+--local typedefs = require "kong.db.schema.typedefs" 
+--
+--local string_array = {
+--  type = "array",
+--  default = {},
+--  elements = { type = "string" },
+--}
+--
+--local test_para = {
+--  type = "record",
+--  fields = {
+--    { test = string_array },
+--  },
+--}
 
 return {
---   name = "kong-plugin-csp",
-   no_consumer = true,
+   name = "kong-plugin-csp",
    fields = {
-      test = {type = "string", required = true}   
---    { consumer = typedefs.no_consumer },
---    { protocols = typedefs.protocols_http },
---    { config = {
---        type = "record",
---        fields = {
---          test = test_para,   
---        },
---      },
---    },
+    {
+      consumer = typedefs.no_consumer
+    },
+    {
+      protocols = typedefs.protocols_http
+    },
+    {
+      config = {
+        type = "record",
+        fields = {
+          {
+            key_names = {
+              type = "array",
+              required = true,
+              elements = typedefs.header_name,
+              default = {
+                "apikey",
+              },
+            },
+          },
+          {
+            hide_credentials = {
+              type = "boolean",
+              default = false,
+            },
+          },
+          {
+            anonymous = {
+              type = "string",
+              uuid = true,
+              legacy = true,
+            },
+          },
+          {
+            key_in_body = {
+              type = "boolean",
+              default = false,
+            },
+          },
+          {
+            run_on_preflight = {
+              type = "boolean",
+              default = true,
+            },
+          },
+        },
+      },
+    },
   },
-}  
+}
