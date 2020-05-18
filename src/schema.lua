ca9216c60c53d5639021b8560b372cda9a50b4e8
focus zheng <zhengfei@ginolegaltech.cn>
2020-05-18 11:14:06 +0800

    update schema

--- src/schema.lua
+++ src/schema.lua
@@ -1,4 +1,4 @@
-local typedefs = require "kong.db.schema.typedefs" 
+--local typedefs = require "kong.db.schema.typedefs" 
 --
 --local string_array = {
 --  type = "array",
@@ -17,48 +17,14 @@ return {
    name = "kong-plugin-csp",
    fields = {
     {
-      consumer = typedefs.no_consumer
-    },
-    {
-      protocols = typedefs.protocols_http
-    },
-    {
       config = {
         type = "record",
         fields = {
           {
             key_names = {
-              type = "array",
-              required = true,
-              elements = typedefs.header_name,
-              default = {
-                "apikey",
-              },
-            },
-          },
-          {
-            hide_credentials = {
-              type = "boolean",
-              default = false,
-            },
-          },
-          {
-            anonymous = {
               type = "string",
-              uuid = true,
-              legacy = true,
-            },
-          },
-          {
-            key_in_body = {
-              type = "boolean",
-              default = false,
-            },
-          },
-          {
-            run_on_preflight = {
-              type = "boolean",
-              default = true,
+              required = true,
+              default = "test"
             },
           },
         },
